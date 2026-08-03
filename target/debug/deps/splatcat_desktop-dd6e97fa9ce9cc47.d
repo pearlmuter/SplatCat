f0bbbff8461a569703bf639424372975fe83f73f
Emil Danielsen <emil.danielsen@gmail.com>
2026-08-03 17:12:45 +0200

    Add gitignore and remove build target artifacts

--- target/debug/deps/splatcat_desktop-dd6e97fa9ce9cc47.d
+++
@@ -1,5 +0,0 @@
-/Users/emil/Documents/Codex/SplatCat/target/debug/deps/splatcat_desktop-dd6e97fa9ce9cc47.d: apps/desktop/src-tauri/src/main.rs
-
-/Users/emil/Documents/Codex/SplatCat/target/debug/deps/libsplatcat_desktop-dd6e97fa9ce9cc47.rmeta: apps/desktop/src-tauri/src/main.rs
-
-apps/desktop/src-tauri/src/main.rs:
